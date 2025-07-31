abc72ea764a5e2d25715ac2068e0f33b2036a719
Delta6626 <hassan911ee@gmail.com>
2025-07-31 23:26:42 +0400

    create plane

--- src/createCube.js
+++ src/createCube.js
@@ -11,15 +11,14 @@ function init() {
   );
   camera.position.x = 0;
   camera.position.y = 0;
-  camera.position.z = 2;
-
-  const plane = createPlane(10, 30, 0xffffff);
-  plane.rotateX(90);
-  scene.add(plane);
+  camera.position.z = 0;
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+  const cuboid = createCuboid(2, 1, 1, 0xfca2a2ff, 0, -2, -3);
+  scene.add(cuboid);
 
   function animate() {
     renderer.render(scene, camera);
@@ -30,13 +29,12 @@ function init() {
 
 init();
 
-function createPlane(width, height, color) {
-  const plane = new THREE.PlaneGeometry(width, height);
-  const material = new THREE.MeshBasicMaterial({
-    color: color,
-    side: THREE.DoubleSide,
-  });
+function createCuboid(width, height, depth, color, posx, posy, posz) {
+  const cuboid = new THREE.BoxGeometry(width, height, depth);
+  const cuboidMaterial = new THREE.MeshBasicMaterial({ color: color });
+  const cuboidMesh = new THREE.Mesh(cuboid, cuboidMaterial);
 
-  const planeMesh = new THREE.Mesh(plane, material);
-  return planeMesh;
+  cuboidMesh.position.set(posx, posy, posz);
+
+  return cuboidMesh;
 }
