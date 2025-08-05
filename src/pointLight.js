9326550fcbe8ef253deb979d52891072576208b3
Delta6626 <hassan911ee@gmail.com>
2025-08-05 17:20:01 +0400

    rename

---
+++ src/pointLight.js
@@ -0,0 +1,46 @@
+import * as THREE from "three";
+import { degToRad } from "three/src/math/MathUtils.js";
+
+function init() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.set(0, 1, 4);
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const pointLight = new THREE.PointLight("rgba(255, 255, 255, 1)", 2);
+  pointLight.position.set(0, 1, 2);
+  scene.add(pointLight);
+
+  const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 256, 256),
+    new THREE.MeshPhongMaterial({ color: "rgba(192, 252, 197, 1)" })
+  );
+
+  const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1000, 1000),
+    new THREE.MeshPhongMaterial({
+      color: "rgba(255, 244, 209, 1)",
+      side: THREE.DoubleSide,
+    })
+  );
+  plane.position.set(0, -1, 0);
+  plane.rotateX(degToRad(90));
+  scene.add(plane);
+
+  scene.add(cube);
+
+  renderer.setAnimationLoop(animate);
+
+  function animate() {
+    renderer.render(scene, camera);
+  }
+}
+
+init();
