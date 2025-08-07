f957c615bfede7ac887da801bf7b5f771ff50351
delta6626 <hassan911ee@gmail.com>
2025-08-07 22:59:12 +0400

    random x position

--- src/main.js
+++ src/main.js
@@ -91,6 +91,10 @@ function createPlane(width, height) {
 
 function animate() {
   controller.update();
+  const cubeGrid = scene.getObjectByName("cubeGrid");
+  cubeGrid.children.forEach((cube) => {
+    //cube.position.x = Math.floor(Math.random() * 10);
+  });
   renderer.render(scene, camera);
 }
 
