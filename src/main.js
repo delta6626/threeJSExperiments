aceda7b7fbedecd1949e118a6c9af2bfafac6691
Delta6626 <hassan911ee@gmail.com>
2025-08-09 20:13:12 +0400

    use orthographic camera

--- src/main.js
+++ src/main.js
@@ -6,12 +6,7 @@ import { Clock } from "three";
 
 function init() {
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+  const camera = new THREE.OrthographicCamera();
   camera.position.set(0, 1, 4);
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
