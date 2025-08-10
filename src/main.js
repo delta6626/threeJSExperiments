ee4abc9cd064da0d8b7267dd6690469a8d666446
Delta6626 <hassan911ee@gmail.com>
2025-08-10 22:08:22 +0400

    swtich to perspective camera

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import { Clock } from "three";
 
 function init() {
   const scene = new THREE.Scene();
-  const camera = new THREE.OrthographicCamera();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
   camera.position.set(0, 1, 4);
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
